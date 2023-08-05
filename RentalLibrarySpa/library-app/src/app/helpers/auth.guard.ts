70d8dd94583ebf99e2adf10167aaf61e1a44d935
anisiceiu <anisictiu@gmail.com>
2023-08-05 17:58:24 +0600

    works update

--- RentalLibrarySpa/library-app/src/app/helpers/auth.guard.ts
+++ RentalLibrarySpa/library-app/src/app/helpers/auth.guard.ts
@@ -19,9 +19,10 @@ export class AuthGuard {
             return true;
         }
          
+        console.log('Home',state);
         this.toastr.error('Access denied', 'Please login to continue access');
         // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        this.router.navigate(['/login']);
         return false;
     }
 }
